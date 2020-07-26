a7b550a751fe516d7ec99efdb7fa5bdef4c38bbb
mkrishnan-codes <manu.krishnan@tarams.com>
2020-07-26 12:10:53 +0530

    todo updated

--- src/components/Todo/todo.js
+++ src/components/Todo/todo.js
@@ -1,21 +1,26 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useReducer } from 'react';
 import './style.scss';
 import Form from './Form';
 let count = 1;
-
+const todosReducer = (state, action) => {
+	switch (action.type) {
+		case 'add':
+			return [...state, { name: action.payload }]
+		case 'mark':
+			return state.map((item, i) => i === action.payload ? { ...item, done: !item.done } : item)
+		default:
+			return state;
+	}
+}
 export const Todo = React.memo(() => {
-	const [items, setitems] = useState([]);
-	const onSubmit = useCallback(item => setitems([...items, { name: item, done: false }]), [items])
-
-	const mark = index => setitems(items.map((item, i) => i === index ? { ...item, done: !item.done } : item))
-
+	const [items, dispatch] = useReducer(todosReducer, [])
 	return (
 		<div className="todo-cont">
-			<Form onSubmit={onSubmit} />
+			<Form onSubmit={(payload) => dispatch({ type: 'add', payload })} />
 			<ul>
 				{
-					items.map((item, i) => <li key={i}
-						onClick={() => mark(i)}
+					items.map((item, payload) => <li key={payload}
+						onClick={() => dispatch({ type: 'mark', payload })}
 						className={item.done ? 'done' : ''}>{item.name}</li>)
 				}
 			</ul>
