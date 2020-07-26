9867a5085b6278d3bff55d01ce978f5053c204ea
mkrishnan-codes <manu.krishnan@tarams.com>
2020-07-26 12:15:30 +0530

    todo fix

--- src/components/Todo/todo.js
+++ src/components/Todo/todo.js
@@ -8,7 +8,7 @@ export const Todo = React.memo(() => {
 	const onSubmit = useCallback(item => setitems([...items, { name: item, done: false }]), [items])
 
 	const mark = index => setitems(items.map((item, i) => i === index ? { ...item, done: !item.done } : item))
-
+	console.log('RRR',count++);
 	return (
 		<div className="todo-cont">
 			<Form onSubmit={onSubmit} />
@@ -19,7 +19,6 @@ export const Todo = React.memo(() => {
 						className={item.done ? 'done' : ''}>{item.name}</li>)
 				}
 			</ul>
-			<h3>Render - {count++}</h3>
 		</div>
 	);
 })
